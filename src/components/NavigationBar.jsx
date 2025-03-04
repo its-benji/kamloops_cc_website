a08802d244fcc261c6e46612951bc0488a52d546
benji <benandrachelward@gmail.com>
2025-03-04 08:07:28 -0800

    swapped for deployment

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,24 +1,15 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import App from "../App";
 
 function NavigationBar() {
 
   return (
     <div>
-        <Router>
-            <nav >
-                <Link to ="/">Home</Link>
-                <Link to ="/about">About Us</Link>
-                <Link to ="/sermons">Sermons</Link>
-                <Link to ="/contact">Contact Us</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/about" element={<App />} />
-                <Route path="/sermons" element={<App />} />
-                <Route path="/contact" element={<App />} />
-            </Routes>
-        </Router>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<App />} />
+          <Route path="/sermons" element={<App />} />
+          <Route path="/contact" element={<App />} />
+      </Routes>
     </div>
   )
 }
